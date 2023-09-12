8116b7f71f8e11cd6a05355a6e741ba56cefe32b
Matthew Hillman <mhillman2@aol.com>
2023-09-12 21:02:41 +0100

    Add project files.

---
+++ CompileTimeRegex.cpp
@@ -0,0 +1,12 @@
+﻿// CompileTimeRegex.cpp : Defines the entry point for the application.
+//
+
+#include "CompileTimeRegex.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
